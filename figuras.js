4ef6f2f00dd650b34a20e82bc74111fe0d72b24c
David Duarte <dhduartea@gmail.com>
2022-06-13 23:33:28 -0500

    Cambio mínimo en la interfaz, y añadir nueva función de  determinar qué tipo de triángulo es el qu se ingresa

--- figuras.js
+++ figuras.js
@@ -2,12 +2,12 @@
 // console.group("Cuadrados")
 // let ladoCuadrado=5;
 // console.log(`Los lados del Cuadrado miden ${ladoCuadrado}cm`);
-const perimetroCuadrado=(ladoCuadrado)=>ladoCuadrado*4;
+const perimetroCuadrado = (ladoCuadrado) => ladoCuadrado * 4;
 
 
 // console.log(`El perímetro es ${perimetroCuadrado(ladoCuadrado)}cm`);
 
-const areaCuadrado=(ladoCuadrado)=>ladoCuadrado**2
+const areaCuadrado = (ladoCuadrado) => ladoCuadrado ** 2
 
 
 // console.log(`El área es ${areaCuadrado(ladoCuadrado)}cm^2`);
@@ -23,14 +23,14 @@ const areaCuadrado=(ladoCuadrado)=>ladoCuadrado**2
 // );
 
 function perimetroTriangulo(ladoTriangulo) {
-    return ladoTriangulo.reduce((previousValue, currentValue) => previousValue+currentValue);
+    return ladoTriangulo.reduce((previousValue, currentValue) => previousValue + currentValue);
 };
 
 // console.log(`El perímetro del Triángulo es ${perimetroTriangulo(ladoTriangulo)}cm`);
 
 function areaTriangulo(ladoTriangulo) {
-    let semiPerimetro=perimetroTriangulo(ladoTriangulo)/2;
-    return Math.sqrt(semiPerimetro*ladoTriangulo.reduce((previousValue,currentValue)=>previousValue*(semiPerimetro-currentValue),1));
+    let semiPerimetro = perimetroTriangulo(ladoTriangulo) / 2;
+    return Math.sqrt(semiPerimetro * ladoTriangulo.reduce((previousValue, currentValue) => previousValue * (semiPerimetro - currentValue), 1));
 }
 
 // console.log(`El área del Triángulo es ${areaTriangulo(ladoTriangulo)}cm^2`);
@@ -41,10 +41,10 @@ function areaTriangulo(ladoTriangulo) {
 // let radioCirculo=5;
 // console.log(`El radio del Círculo es ${radioCirculo} y su diámetro es ${radioCirculo*2}`);
 
-const perimetroCirculo=(radioCirculo)=>radioCirculo*2*Math.PI;
+const perimetroCirculo = (radioCirculo) => radioCirculo * 2 * Math.PI;
 // console.log(`El perímetro del Círculo es ${perimetroCirculo(radioCirculo)}cm`);
 
-const areaCirculo =(radioCirculo)=>Math.PI*Math.pow(radioCirculo,2);
+const areaCirculo = (radioCirculo) => Math.PI * Math.pow(radioCirculo, 2);
 
 // console.log(`El área del Círculo es ${areaCirculo(radioCirculo)}cm^2`);
 // console.groupEnd();
@@ -52,29 +52,58 @@ const areaCirculo =(radioCirculo)=>Math.PI*Math.pow(radioCirculo,2);
 //HTML
 
 function calcularAreaPerimetroCuadrado() {
-    const input=document.getElementById("ladoCuadrado");
-    const value=input.value;
+    const input = document.getElementById("ladoCuadrado");
+    const value = input.value;
 
-    const perimetro=perimetroCuadrado(value);
-    const area=areaCuadrado(value);
+    const perimetro = perimetroCuadrado(value);
+    const area = areaCuadrado(value);
     alert(`El cuadrado tiene un perímetro es ${perimetro}cm y su área es ${area}cm^2`);
 }
 
 function calcularAreaPerimetroTriangulo() {
-    let valor=[];
-    valor.push(parseInt(document.getElementById("ladoTriangulo1").value));
-    valor.push(parseInt(document.getElementById("ladoTriangulo2").value));
-    valor.push(parseInt(document.getElementById("ladoTriangulo3").value));
-    
-    const perimetro=perimetroTriangulo(valor);
-    const area=areaTriangulo(valor);
+    let valor = parseToNumberTriangle();
+    const perimetro = perimetroTriangulo(valor);
+    const area = areaTriangulo(valor);
     alert(`El Triangulo tiene un perímetro es ${perimetro}cm y su área es ${area}cm^2`);
 }
 function calcularAreaPerimetroCirculo() {
-    const input=document.getElementById("radioCirculo");
-    const value=input.value;
+    const input = document.getElementById("radioCirculo");
+    const value = input.value;
 
-    const perimetro=perimetroCirculo(value);
-    const area=areaCirculo(value);
+    const perimetro = perimetroCirculo(value);
+    const area = areaCirculo(value);
     alert(`El círculo tiene un perímetro es ${perimetro}cm y su área es ${area}cm^2`);
+}
+//Reto, determinar qué tipo de triángulo es 
+function tipoTriangulo() {
+    let valor = parseToNumberTriangle();
+    let altura;
+    if ((valor[0] === valor[1]) && (valor[0] === valor[2])) {
+        altura = alturaTrianguloEquilateroIsosceles(valor[0], valor[0])
+        alert(`Triángulo Equilatero y su altura es ${altura}`)
+
+    } else if (valor[0] === valor[1]) {
+
+        altura = alturaTrianguloEquilateroIsosceles(valor[0], valor[2])
+        alert(`Triángulo Isósceles y su altura es ${altura}`)
+
+    } else if (valor[0] === valor[2]) {
+        altura = alturaTrianguloEquilateroIsosceles(valor[0], valor[1])
+        alert(`Triángulo Isósceles y su altura es ${altura}`)
+
+    } else {
+        alert(`Triángulo Escaleno y su altura depende de qué lado se escoja como base`)
+
+    }
+}
+function alturaTrianguloEquilateroIsosceles(lado, base) {
+    let salida = (lado ** 2 - (base / 2) ** 2) ** (1 / 2);
+    return salida;
+}
+function parseToNumberTriangle() {
+    let valor = [];
+    valor.push(parseInt(document.getElementById("ladoTriangulo1").value));
+    valor.push(parseInt(document.getElementById("ladoTriangulo2").value));
+    valor.push(parseInt(document.getElementById("ladoTriangulo3").value));
+    return valor;
 }
