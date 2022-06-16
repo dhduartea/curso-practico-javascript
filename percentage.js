8de354c46de7025fd477d7ede59490f9b5e669de
David Duarte <dhduartea@gmail.com>
2022-06-16 11:14:27 -0500

    Creación del la sección de porcentaje y su implementación básica con Css y Js

---
+++ percentage.js
@@ -0,0 +1,6 @@
+function calcularDescuento(){
+    let precioTotal=parseInt(document.getElementById("totalPrice").value);
+    let porcentaje=parseInt(document.getElementById("percentageDiscount").value);
+    let descuento=precioTotal*(1-porcentaje/100)
+    alert(`EL precio final del producto es ${descuento}`)
+}
